981f3f7b2faad3e6a1cb9eb789bdb0488ee64042
Theoutrace <prakashkumar.imw@gmail.com>
2023-01-05 02:16:28 +0530

    clean code by exporting routes

--- app.js
+++ app.js
@@ -1,48 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  //   console.log(req.url, req.method, req.headers);
-  //   process.exit();
+const routes = require("./routes");
 
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-
-      console.log("data frm file: ", data);
-      res.write("<html>");
-      res.write("<head><title>My Page</title></head>");
-      res.write(
-        `<body>${data}<form action="/message" method="POST" ><input type="text" name="message"><button type="submit">send</button></form></body>`
-      );
-      res.write("</html>");
-      return res.end();
-    });
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log("chunk is :", chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log("parsedBody", parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        // console.log(err);
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("data frm file: ", data);
+      res.write("<html>");
+      res.write("<head><title>My Page</title></head>");
+      res.write(
+        `<body>${data}<form action="/message" method="POST" ><input type="text" name="message"><button type="submit">send</button></form></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk is :", chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log("parsedBody", parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        // console.log(err);
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+};
+
+module.exports = requestHandler;
