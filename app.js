b10e0e7bd4f1c726094da2367a15a3b0e86cbb31
Theoutrace <prakashkumar.imw@gmail.com>
2023-01-05 01:34:55 +0530

    wrote the file and read file print in UI

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   //   console.log(req.url, req.method, req.headers);
   //   process.exit();
 
   const url = req.url;
-  if (url === "/home") {
-    res.setHeader("Content-type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Page</title></head>");
-    res.write("<body><h1>Welcome home</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/about") {
-    res.setHeader("Content-type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Page</title></head>");
-    res.write("<body><h1>Welcome to About Us page</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/node") {
-    res.setHeader("Content-type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Page</title></head>");
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-    res.write("</html>");
-    return res.end();
+  const method = req.method;
+
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log("data frm file: ", data);
+      res.write("<html>");
+      res.write("<head><title>My Page</title></head>");
+      res.write(
+        `<body>${data}<form action="/message" method="POST" ><input type="text" name="message"><button type="submit">send</button></form></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
   }
-  res.setHeader("Content-type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My Page</title></head>");
-  res.write("<body><h1>Hello from my Node js server!</h1></body>");
-  res.write("</html>");
-  res.end();
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk is :", chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log("parsedBody", parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        // console.log(err);
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
 });
 
 server.listen(3000);
