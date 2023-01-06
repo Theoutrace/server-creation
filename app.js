b81ebc55ed10c121317bbe1c54bbb959d62a5274
Theoutrace <prakashkumar.imw@gmail.com>
2023-01-06 16:39:41 +0530

    body parser, add product

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("in the middleware!");
-  next();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' ><input type='text' name='size' placeholder='size'><button type='submit'>Add Product</button></form>"
+  );
 });
-app.use((req, res, next) => {
-  console.log("in the 2nd middleware!");
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1> hello to node js </h1>");
-  //   res.send({ key1: value });
 });
 
 app.listen(3000);
